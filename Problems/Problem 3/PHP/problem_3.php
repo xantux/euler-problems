9196c2b713dc01ebc2e4ddbd2d45be1b86bb821f
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-04-18 15:40:51 +0200

    Problem 3 PHP procedure

---
+++ Problems/Problem 3/PHP/problem_3.php
@@ -0,0 +1,43 @@
+<?php
+/**********************************
+ * 
+ * Problem 3 from Project Euler
+ * 
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ *  
+ * PLEASE NOTE
+ * In order to test this script you must have PHP 5.3+ installed and php-cli, to test it with a terminal.
+ *
+ * REGARDING THE PERFORMANCE
+ * This is not the final version, as you can see I'll trace the version of every solution, so please don't think that
+ * this is the best algorithm in terms of speed and code optimization, for any trouble open an issue.
+ * 
+ * @author Claudio Ludovico Panetta (@ludo237)
+ * @version 1.0.0
+ **************************************/
+
+// Starting the benchmark
+$startTime = microtime();
+
+// Our starting point is a specific number but we can play with whatever
+// number we want
+$number = 600851475143; 
+
+// We need an array to store the prime factors
+$primeFactors = array();
+
+for($i = 2; $i <= $number; $i++) {
+    if( $number % $i == 0) {
+        $number /= $i;
+        $primeFactors[] = $i;
+    }
+}
+
+// with PHP we can use a builtin function to find the large prime factor
+// but with other language you have to code it.
+max($primeFactors);
+
+// Finish the benchmark 
+$endTime = microtime();
+echo "Algorithm time: ". ($endTime - $startTime);
