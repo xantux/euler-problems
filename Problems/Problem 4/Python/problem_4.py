935bb836f090a1bd2d1d81a8badc29962096a104
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-04-18 19:15:12 +0200

    Refactoring a bit Python Problem 4. Not finished.

--- Problems/Problem 4/Python/problem_4.py
+++ Problems/Problem 4/Python/problem_4.py
@@ -1,10 +1,26 @@
 '''
+
+Problem 4 from Project Euler
+
 A palindromic number reads the same both ways. The largest palindrome made from
-the product of two 2-digit numbers is 9009 = 91 × 99.
+the product of two 2-digit numbers is '9009' = '91' times '99'.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
+
+PLEASE NOTE
+In order to work you should have at least Python 2.3+ install, 2.7+ is reccomended.
+
+REGARDING THE PERFORMANCE
+This is not the final version of the algorithm, everyone can improve it.
+
+@author
+@author Claudio Ludovico Panetta (@Ludo237)
+
+@version 1.0.1
+
 '''
 
+# Support function to check if a number is palindromic
 def isPalindromic(n):
     n = str(n)
     for i in range(len(n)//2):
@@ -12,12 +28,12 @@ def isPalindromic(n):
             return False
     return True
 
+
 maxPalidomic = 0
+
 for i in range(1000,100,-1):
     for j in range(1000, 100,-1):
         if isPalindromic(i*j):
             if i*j>maxPalidomic:
                 maxPalidomic = i*j
-
 print(maxPalidomic)
-
