83a4b7cf168bdc5b20cc3b48c3960de44019dc7c
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-04-19 10:55:23 +0200

    [ Add ] C procedure for Problem 3, NOT FINISHED

---
+++ Problems/Problem 3/C/problem_3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/****************************************
+ *
+ * Problem 3 from Project Euler
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ *
+ * PLEASE NOTE 
+ * In order to compile this file you must have gcc installed
+ *
+ * REGARDING THE PERFORMANCE
+ * This is not the final version of the scripts, you can improve the algorithm by submitting a pull request or an issue on Github.
+ * Feel free to ask anything on Github.
+ *
+ * @author Claudio Ludovico Panetta (@Ludo237)
+ * @version 1.0.0
+ *******************************************/
