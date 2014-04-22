5f61bb9778675e10e5e692beb652a17c065b7814
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-04-22 11:26:36 +0200

    [ Update ] Problem 1 PHP Procedure with Gauss
    English translation, that’s all.

--- Problems/Problem 1/PHP/problem_1_gauss.php
+++ Problems/Problem 1/PHP/problem_1_gauss.php
@@ -15,88 +15,106 @@
  * This is not the final version, as you can see I'll trace the version of every solution, so please don't think that
  * this is the best algorithm in terms of speed and code optimization, for any trouble open an issue.
  *
+ * @author Pietro Arturo Panetta (arturu.it)
  * @author Claudio Ludovico Panetta (@Ludo237)
- * @version 1.5.0
+ * @version 1.5.1
  *************************************/
-/**
- * Ringrazio Gauss per la soluzione
- */
+
+// This solution involing Gauss
 
 // Start the benchmark
 $startTest = microtime();
 
 // We need a scalable algorithm so we cannot use a static zise for our test
-$fine = 1000;
-$inizio = 0;
+$end = 1000;
+$start = 0;
 
-// abbasso di uno la $fine perché nelle richieste i multipli di x
-// devono essere inferiori a $fine;
---$fine;
+// Lower the end by a factor of one in order to server all the multiples of x
+// minus the $end
+--$end;
 
-/** --- Funzioni ---- **/
-// NB alto() e basso() sono simili, ne ho scritte due, sono più leggibili
+/** --- Functions ---- **/
+// Please Note high() and lower() are strictly similar, but if we keep them separate they
+// are more readable
 
-// trovo il multiplo più alto
-// il numero di iterazioni del for al massimo è uguale alla ragione
-function alto ($ragione,$fine) {
-	// partendo dal numero più alto diminuisco di una unità
-	// finché non trovo il multiplo più alto
-	for ( $i = $fine; ; $i--){
-		if ( $i % $ragione == 0 )
+/**
+ * function higher
+ * @param $end
+ * @param $reason
+ * @description
+ *  Find the higher multiple
+ **/
+function higher ($reason, $end) {
+    // Starting from the higher i lower by a factor of one
+    // untile we found the higher multiple
+	for ( $i = $end; ; $i--){
+		if ( $i % $reason == 0 )
 			return $i;
 	}
 }
 
-// trovo il multiplo più basso
-// (se si parte da 0 è uguale alla "ragione" della progressione)
-// il numero di iterazioni del for al massimo è uguale alla ragione
-function basso ($ragione,$inizio){
-	// in caso inizio è zero
-	$inizio = ($inizio==0) ? ++$inizio : $inizio;
+/**
+ * function lower
+ * @param $start
+ * @param $reason
+ * @description
+ *  Find the lower multilple
+ **/
+function lower ($reason, $start){
+	// if start is zero we need to fix it
+	$start = ($start == 0) ? ++$start : $start;
 
-	// partendo dall'inizio della serie
-	// aumento di una unità finché non trovo il primo multiplo
-	for ($i=$inizio; ; $i++){
-		if ( $i % $ragione == 0 )
+    // Starting from the beginning
+    // we add by a factor of one until we found the lower multiple
+	for ($i = $start; ; $i++){
+		if ( $i % $reason == 0 )
 			return $i;
 	}
 }
 
-/** --------- numero iterazioni -------- **/
-// il numero di iterazioni è quante volte il multiplo si trova nella serie
-// Es: count ( 3 + 3 + 3 + ... + (n-3) + (n-2) + (n-1) + (n-0) )
-// per una data ragione trovo il numero di iterazioni
-function iterazioni ($ragione,$alto,$basso){
-	// restituisco il numero di iterazioni
-	// quando $inizio==0 -> (n-1)
-	return $alto/$ragione;
+/**
+ * function iterations
+ * @param $lower
+ * @param $higher
+ * @param $reason
+ * @description
+ *  Iterations is how many times we found the multilple inside our serie
+ **/
+function iterations ($reason, $higher, $lower){
+    // return the number of iterations
+    // when $start == 0 -> (n - 1)
+    return $higher / $reason;
 }
 
 
-/** Trova l'area del trapezio **/
-// grazie al "Principe dei matematici": Gauss
-// la somma dei numeri di una progressione artimentica è uguale
-// all'area del trapezio
-function areaTrapezio($ragione,$inizio,$fine){
-	$base_maggiore = alto($ragione,$fine);
-	$base_minore = basso($ragione,$inizio);
+/**
+ * function trapeziusArea
+ * @param $end
+ * @param $start
+ * @param $reason
+ * @description
+ *  Thanks to Gauss and his idea where the sum of numbers inside
+ *  a progression is equal to trapezius area we can identify our
+ *  result base on it.
+ **/
+function trapeziusArea($reason, $start, $end) {
+	$majorBase = higher($reason, $end);
+	$minorBase = lower($reason,$start);
 	// 3*1 + 3*2 + 3*3 + ... 3(n-3) + 3(n-2) + 3(n-1) + 3(n-0)
-	// uguare ad: altreza * (base_maggiore + base_minore) / 2
-	return
-		iterazioni($ragione,$base_maggiore,$base_minore) *
-		($base_maggiore + $base_minore)
-		/ 2;
+    // equal to: h * (majorBase + minorBase) / 2
+    // Awesome!
+	return iterations($reason, $majorBase, $minorBase) * ($majorBase + $minorBase) / 2;
 }
 
-/** ---- Calcolo le Aree delle diverse sequenze ----- **/
-$tre_somma = areaTrapezio(3,$inizio,$fine);
+/** ---- Calculating areas of different sequences ----- **/
+$sumOfThree = trapeziusArea(3, $start, $end);
 
-$cinque_somma = areaTrapezio(5,$inizio,$fine);
+$sumOfFive = trapeziusArea(5, $start, $end);
 
-$multipli_coincidenti = areaTrapezio(3*5,$inizio,$fine);
+$overlappingMultiples = trapeziusArea( 3*5 ,$start, $end);
 
 
-$total = $tre_somma + $cinque_somma - $multipli_coincidenti;
+$total = $sumOfThree + $sumOfFive - $overlappingMultiples;
 
 // End of benchmark
 $endTest = microtime();
